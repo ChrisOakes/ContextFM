483aa20ea21b8c5a4380799ea6e52c6ef48e401b
Christopher Oakes <49435988+ChrisOakes@users.noreply.github.com>
2022-09-25 16:22:13 +0100

    Addition of circle, triangle and square methods

---
+++ Start/ContextFM_Demo/ContextFM.Services/ShapesCalculationService.cs
@@ -0,0 +1,65 @@
+﻿namespace ContextFM.Services
+{
+    public class ShapesCalculationService
+    {
+        //Square
+        public decimal CalculateAreaSquare(decimal breadth, decimal height)
+        {
+            decimal area = 0;
+
+            area = breadth * height;
+
+            return area;
+        }
+
+        //Square
+        public decimal CalculatePerimeter(decimal breadth, decimal height)
+        {
+            decimal perimeter = 0;
+
+            perimeter = 2 * (breadth) + 2 * (height);
+
+            return perimeter;
+        }
+
+        //Circle
+        public decimal CalculateArea(decimal radius)
+        {
+            decimal area = 0;
+
+            area = Convert.ToDecimal(Math.Pow(Math.PI * Convert.ToDouble(radius), 2));
+
+            return area;
+        }
+
+        //Circle
+        public decimal CalculatePerimeter(decimal radius)
+        {
+            decimal perimeter = 0;
+
+            perimeter = Convert.ToDecimal(2 * Math.PI * Convert.ToDouble(radius));
+
+            return perimeter;
+        }
+
+        //Triangle
+        public decimal CalculateAreaTriangle(decimal breadth, decimal height)
+        {
+            decimal area = 0;
+
+            area = (breadth * height) / 2;
+
+            return area;
+        }
+
+        //Triangle
+        public decimal CalculatePerimeter(decimal sideA, decimal sideB, decimal sideC)
+        {
+            decimal perimeter = 0;
+
+            perimeter = sideA + sideB + sideC;
+
+            return perimeter;
+        }
+    }
+}
